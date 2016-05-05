5cd7442be5ca2755ca621c5f3ec1c30f01b66c6e
Michael Cola <michaelc@procore.com>
2016-05-05 11:39:33 -0500

    Added species list

--- rananasus/app/models/physical_attributes.rb
+++ rananasus/app/models/physical_attributes.rb
@@ -2,4 +2,6 @@ class PhysicalAttributes < ActiveRecord::Base
   belongs_to :species
 
   validates :diet, :blood_type, :skin_type, :color, :size, :shape, presence: true
+
+  
 end
--- rananasus/app/models/species.rb
+++ rananasus/app/models/species.rb
@@ -2,4 +2,14 @@ class Species < ActiveRecord::Base
   belongs_to :genus
 
   validates :name, presence: true
+
+  ALL_SPECIES = [
+    :rananasus,
+    :tuna,
+    :leopard,
+    :tiger,
+    :tyrannasaurus,
+    :indominus,
+    :pteradactyl
+  ]
 end
