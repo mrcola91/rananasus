b14cc1ef167d4e5cd9e2ff4299f29355d462b07f
Michael Cola <michaelc@procore.com>
2016-05-05 11:46:00 -0500

    Add action for fusion controller

--- rananasus/app/controllers/fusion_controller.rb
+++ rananasus/app/controllers/fusion_controller.rb
@@ -1,3 +1,10 @@
 class FusionController < ApplicationController
+  def fuse
+    genus = params[:genus]
+    first_species = params[:species][0]
+    second_species = params[:species][1]
 
+    result = first_species.slice((species.length / 2).to_i) + second_species((species.length / 2).to_i)
+    render result
+  end
 end
